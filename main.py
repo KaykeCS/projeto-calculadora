1cd509ffebe0fd69f8c119f98083ed43ce8290b4
Kayke CS <kaykecsgit@gmail.com>
2023-11-30 17:38:18 -0300

    adição de lista de números e clear nos calculos coletados.

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
-addition = "+"
-subtraction = "-"
-multiplication = "*"
-division = "/"
+array_numbers = []
+result = None
 
 
 def Calculator():
-    n1 = int(input("Digite um número: "))
+    global result
+
+    n1 = float(input("Digite um número: "))
+    array_numbers.append(n1)
 
     quest_operator = input(
-        "Digite o operador que deseja utilizar: (+) (-) (*) (/) \n"
+        "Digite o operador que deseja utilizar -> (+) (-) (*) (/) :\n "
     )
+    array_numbers.append(quest_operator)
 
-    result = None
-    n2 = int(input("Digite outro número: "))
+    n2 = float(input("Digite outro número: "))
+    array_numbers.append(n2)
 
-    if quest_operator == addition:
-        result = n1 + n2
-    elif quest_operator == subtraction:
-        result = n1 - n2
-    elif quest_operator == multiplication:
-        result = n1 * n2
-    elif quest_operator == division:
-        result = n1 / n2
+    if len(array_numbers) >= 3:
+        n1 = array_numbers[0]
+        quest_operator = array_numbers[1]
+        n2 = array_numbers[2]
+
+        if quest_operator == '+':
+            result = n1 + n2
+        elif quest_operator == '-':
+            result = n1 - n2
+        elif quest_operator == '*':
+            result = n1 * n2
+        elif quest_operator == '/':
+            if n2 == 0:
+                print("Não é possível dividir por zero.")
+            else:
+                result = n1 / n2
+        else:
+            print("Operador inválido.")
+
+        if result is not None:
+            print(f"Resultado: {result}")
     else:
-        print("Operador inválido.")
-
-    if result is not None:
-        print(f"Resultado: {result}")
+        print("Não há números suficientes na lista para calcular.")
 
 
-ex_calculator = Calculator()
+def Delete_calcs():
+    del_calc = input('Deseja apagar os cálculos ? \n (sim)  ou  (não) : ')
+
+    if del_calc.lower() == 'sim':
+        array_numbers.clear()
+        print("Cálculos apagados.")
+    else:
+        print("Cálculos não foram apagados.")
+
+
+Calculator()
+Delete_calcs()
