3992f970ee7e6e6f3af6b1743cd2df9ae73b5bf8
Kayke CS <kaykecsgit@gmail.com>
2023-12-04 16:18:19 -0300

    adicionando dois novos operadores, exponenciação e raiz quadrada utilizando a biblioteca math.

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
+import math
+
 array_numbers = []
 result = None
 
 
 def get_number():
-    try:
-        num = float(input("Digite um número: "))
-        return num
-    except ValueError:
-        print('Dados inválidos. Insira apenas números.')
-        return None
+    while True:
+        try:
+            num = float(input("Digite um número: "))
+            return num
+        except ValueError:
+            print('Dado inválido. Insira apenas números.')
 
 
 def get_operator():
     while True:
         operator = input(
-            "Digite o operador que deseja utilizar (+) (-) (*) (/): ")
-        if operator in ('+', '-', '*', '/'):
+            "Digite o operador que deseja utilizar : ")
+        if operator in ('+', '-', '*', '/', '**', 'raiz'):
             return operator
         else:
             print('Operador inválido. Insira apenas os operadores válidos.')
 
 
-def calculate(num1, operator, num2):
+def calculate(num1, operator, num2=None):
     if operator == '+':
         return num1 + num2
     elif operator == '-':
@@ -30,38 +32,41 @@ def calculate(num1, operator, num2):
         return num1 * num2
     elif operator == '/':
         if num2 == 0:
-            print("Não é possível dividir por zero.")
+            print("Erro: Divisão por zero.")
             return None
         else:
             return num1 / num2
+    elif operator == '**':
+        return num1 ** num2
+    elif operator == 'raiz':
+        return math.sqrt(num1)
 
 
 def Calculator():
     global result
-
     num1 = get_number()
+
     if num1 is None:
         return
-    array_numbers.append(num1)
 
+    array_numbers.append(num1)
     operator = get_operator()
     array_numbers.append(operator)
 
-    num2 = get_number()
-    if num2 is None:
-        return
-    array_numbers.append(num2)
-
-    if len(array_numbers) >= 3:
-        num1 = array_numbers[0]
-        operator = array_numbers[1]
-        num2 = array_numbers[2]
-
-        result = calculate(num1, operator, num2)
-        if result is not None:
-            print(f"Resultado: {result}")
+    if operator != 'raiz':
+        num2 = get_number()
+        if num2 is None:
+            return
+        array_numbers.append(num2)
     else:
-        print("Não há números suficientes na lista para calcular.")
+        array_numbers.append(None)
+
+    num1 = array_numbers[0]
+    operator = array_numbers[1]
+
+    result = calculate(num1, operator, array_numbers[2])
+    if result is not None:
+        print(f"Resultado: {result}")
 
 
 while True:
