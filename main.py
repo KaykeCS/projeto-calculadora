5d9877b7321f661d1813572309b9b1a0b0380dff
Kayke CS <kaykecsgit@gmail.com>
2023-12-04 14:51:18 -0300

    adicionando funções menores e separadas e com mais tratamentos de erros.

--- main.py
+++ main.py
@@ -2,67 +2,71 @@ array_numbers = []
 result = None
 
 
+def get_number():
+    try:
+        num = float(input("Digite um número: "))
+        return num
+    except ValueError:
+        print('Dados inválidos. Insira apenas números.')
+        return None
+
+
+def get_operator():
+    while True:
+        operator = input(
+            "Digite o operador que deseja utilizar (+) (-) (*) (/): ")
+        if operator in ('+', '-', '*', '/'):
+            return operator
+        else:
+            print('Operador inválido. Insira apenas os operadores válidos.')
+
+
+def calculate(num1, operator, num2):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 == 0:
+            print("Não é possível dividir por zero.")
+            return None
+        else:
+            return num1 / num2
+
+
 def Calculator():
     global result
 
-    try:
-        n1 = float(input("Digite um número: "))
-    except ValueError:
-        print('Dados inválidos. Insira apenas números.')
+    num1 = get_number()
+    if num1 is None:
         return
+    array_numbers.append(num1)
 
-    array_numbers.append(n1)
+    operator = get_operator()
+    array_numbers.append(operator)
 
-    quest_operator = input(
-        "Digite o operador que deseja utilizar -> (+) (-) (*) (/) :\n "
-    )
-    if quest_operator not in ('+', '-', '*', '/'):
-        print('Operador inválido. Insira apenas os operadores válidos.')
+    num2 = get_number()
+    if num2 is None:
         return
-    array_numbers.append(quest_operator)
-
-    try:
-        n2 = float(input("Digite um número: "))
-    except ValueError:
-        print('Dados inválidos. Insira apenas números.')
-        return
-
-    array_numbers.append(n2)
+    array_numbers.append(num2)
 
     if len(array_numbers) >= 3:
-        n1 = array_numbers[0]
-        quest_operator = array_numbers[1]
-        n2 = array_numbers[2]
+        num1 = array_numbers[0]
+        operator = array_numbers[1]
+        num2 = array_numbers[2]
 
-        if quest_operator == '+':
-            result = n1 + n2
-        elif quest_operator == '-':
-            result = n1 - n2
-        elif quest_operator == '*':
-            result = n1 * n2
-        elif quest_operator == '/':
-            if n2 == 0:
-                print("Não é possível dividir por zero.")
-            else:
-                result = n1 / n2
-        else:
-            print("Operador inválido.")
-
+        result = calculate(num1, operator, num2)
         if result is not None:
             print(f"Resultado: {result}")
     else:
         print("Não há números suficientes na lista para calcular.")
 
 
-def Delete_calcs():
-    del_calc = input('Deseja apagar os cálculos ? \n (sim)  ou  (não) : ')
+while True:
+    Calculator()
 
-    if del_calc.lower() == 'sim':
-        array_numbers.clear()
-        print("Cálculos apagados.")
-    else:
-        print("Cálculos não foram apagados.")
-
-
-Calculator()
-Delete_calcs()
+    yes_no = input("Deseja continuar calculando? (sim/não): ")
+    if yes_no.lower() != 'sim':
+        break
